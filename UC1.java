6308ec69059fd1bf31a8c82db5c672144d1f4e31
Shridhar  Kolhatkar <88042097+Shridhar2211@users.noreply.github.com>
2021-09-30 20:02:00 +0530

    Added: creating a board

--- UC1.java
+++ UC1.java
@@ -1,16 +1,16 @@
-package program:
+package program;
 public class TicTacToeGame {
 	//creating a instance variable  and asigning value 10
 	static char[] board =new char[10];
 	public static void main (String [] args)
 	{
-	GameBoard();
+	GameBoard();// calling the game board method
 	}
-	public static void GameBoard()
+	public static void GameBoard() //method is defined for creating board
 	{
-	for (int i=1;  i<=board.length; i++)
+	for (int i=1;  i<board.length; i++)
 	{
-	System.out.println(" "+i);	
+	System.out.println(" ");	
 		}
 	}
 }
